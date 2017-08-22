0c71d471a19af1f9308719f9f4adba809227ac3e
Theophile <theophile@mediateca-06.institutfrancais.es>
2017-08-22 13:09:19 +0200

    first deploy

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ app.use(express.static(__dirname + '/dist'));
 // a protocol other than HTTPS,
 // redirect that request to the
 // same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
+// // Instruct the app
+// // to use the forceSSL
+// // middleware
+// app.use(forceSSL());
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
