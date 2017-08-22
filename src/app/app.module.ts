60423778192a42685f17c95460235ec8039df7e7
Theophile <theophile@iMac-de-administrador-3.local>
2017-08-22 16:30:39 +0200

    first deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,3 +26,54 @@ import {CdkTableModule} from '@angular/cdk';
 export class AppModule { 
 
 }
+
+// import { NgModule }      from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule }   from '@angular/forms';
+// import { HttpModule }    from '@angular/http';
+
+// import { FormComponent } from './login/form.component';
+// import { AppRoutingModule } from './app-routing.module';
+
+// import { MasonryModule } from 'angular2-masonry';
+// //component
+// import { AppComponent }         from './app.component';
+// import { BearComponent }   from './liste/bear.component';
+// import { HomeComponent }  from './home/home.component';
+// import { BearDetailComponent }  from './detail/bear-detail.component';
+// import { visucanvasComponent }  from './visualizer/visucanvas.component'; 
+// import { LandingPageComponent }  from './home/landingpage.component';
+// import { AdminComponent }  from './admin/admin.component';
+// import { LoginComponent }  from './login/login.component';
+// import { visucanvasGreenComponent }  from './visualizer/greenvisualizer.component'; 
+// //service
+// import { BearService }          from './bear.service';
+
+// import { UserService } from './user.service';
+// import { LoggedInGuard } from './logged-in.guard';
+
+// @NgModule({
+//   imports: [
+//   BrowserModule,
+//   FormsModule,
+//   HttpModule,
+//     AppRoutingModule,
+//     MasonryModule
+//     ],
+//     declarations: [
+//     AppComponent,
+//     BearComponent,
+//     FormComponent,
+//     HomeComponent,
+//     BearDetailComponent,
+//     LandingPageComponent,
+//     visucanvasComponent,
+//     AdminComponent,
+//     LoginComponent,
+//     visucanvasGreenComponent
+
+//     ],
+//     providers: [ BearService , UserService, LoggedInGuard ],
+//     bootstrap: [ AppComponent ]
+//   })
+// export class AppModule { }
